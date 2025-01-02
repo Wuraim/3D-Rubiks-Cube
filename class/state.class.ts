f6f6566547d1b80463e9d50bdd8e237017de80eb
Wuraim <lorgnierg@yahoo.com>
2025-01-02 17:39:01 +0100

    state class refacto display

--- class/state.class.ts
+++ class/state.class.ts
@@ -11,42 +11,6 @@ interface StateFaceRotation {
 
 export default class State {
 
-    Up = [
-        ['W', 'W', 'W'],            
-        ['W', 'W', 'W'],
-        ['W', 'W', 'W']
-    ];
-    
-    Down = [
-        ['Y', 'Y', 'Y'],
-        ['Y', 'Y', 'Y'],
-        ['Y', 'Y', 'Y']
-    ];
-    
-    Front = [
-        ['R', 'R', 'R'], // Rouge en face avant
-        ['R', 'R', 'R'],
-        ['R', 'R', 'R']
-    ];
-
-    Back = [
-        ['O', 'O', 'O'], // Orange en face arrière
-        ['O', 'O', 'O'],
-        ['O', 'O', 'O']
-    ];
-    
-    Left = [
-        ['B', 'B', 'B'], // Bleu à gauche
-        ['B', 'B', 'B'],
-        ['B', 'B', 'B']
-    ];
-    
-    Right = [
-        ['G', 'G', 'G'], // Vert à droite
-        ['G', 'G', 'G'],
-        ['G', 'G', 'G']
-    ];
-
     cubeSolver = new CubeJS();
 
     constructor() {
@@ -69,12 +33,14 @@ export default class State {
         return '\x1b[30m|\x1b[0m';
     }
 
-    getFaceDisplay(face: Face): Array<string> {
+    getFaceDisplay(start: number): Array<string> {
         let result: Array<string> = [];
+        const face = this.getFaceFromSolverState(start);
         face.forEach((line) => {
             const coloredLine = line.map(cubie => this.getColoredCubie(cubie)).join(this.getPipe());
             result.push(this.getPipe() + coloredLine + this.getPipe());
         });
+
         return result;
     }
 
@@ -96,19 +62,30 @@ export default class State {
         finalDisplay.forEach((line) => console.log(line));
     }
 
+    getFaceFromSolverState(start:number): Face {
+        let result = [];
+        const solverState: string = this.cubeSolver.asString();
+
+        for (let i = 0; i < 3; i++) {
+            const line = [];
+            for (let j = 0; j < 3; j++) {
+                const letter = solverState[start + i*3 + j];
+                line.push(letter)
+            }
+            result.push(line);
+        }
+        console.log(start, result)
+        return result;
+    }
+
     showState(){
-        /*
-        
-
-        const faceUp = solverState.substring(0,9)
-
-        let firstRowOfDisplay = [this.getSpaceDisplay(), this.getFaceDisplay(this.Up)]
+        let firstRowOfDisplay = [this.getSpaceDisplay(), this.getFaceDisplay(0)]
         let firstRow = firstRowOfDisplay.reduce((acc, val) => this.concatFaceDisplay(acc,val))
 
-        let allFace = [this.Left, this.Front, this.Right, this.Back]
-        let secondRow = allFace.map((face) => this.getFaceDisplay(face)).reduce((acc, val) => this.concatFaceDisplay(acc,val));
+        let allFace = [36, 18, 9, 45]
+        let secondRow = allFace.map((index) => this.getFaceDisplay(index)).reduce((acc, val) => this.concatFaceDisplay(acc,val));
 
-        let thirdRowOfDisplay = [this.getSpaceDisplay(), this.getFaceDisplay(this.Down)]
+        let thirdRowOfDisplay = [this.getSpaceDisplay(), this.getFaceDisplay(27)]
         let thirdRow = thirdRowOfDisplay.reduce((acc, val) => this.concatFaceDisplay(acc,val))
 
         console.log('')
@@ -116,9 +93,6 @@ export default class State {
         this.logDisplay(secondRow)
         this.logDisplay(thirdRow)
         console.log('')
-        */
-        const solverState: string = this.cubeSolver.asString();
-        console.log(solverState)
     }
 
     rotation: Array<StateFaceRotation> = [
@@ -150,217 +124,5 @@ export default class State {
     
         this.showState();
     }
-    
-
-    rotateFaceClockwise(face: Face): Face {
-        const N = face.length;
-        const newFace = Array.from({ length: N }, () => Array(N).fill(''));
-    
-        for (let i = 0; i < N; i++) {
-            for (let j = 0; j < N; j++) {
-                newFace[j][N - 1 - i] = face[i][j];
-            }
-        }
-    
-        return newFace;
-    }
-
-    rotateFaceCounterClockwise(face: Face): Face {
-        const N = face.length;
-        const newFace = Array.from({ length: N }, () => Array(N).fill(''));
-    
-        for (let i = 0; i < N; i++) {
-            for (let j = 0; j < N; j++) {
-                newFace[N - 1 - j][i] = face[i][j];
-            }
-        }
-    
-        return newFace;
-    }
-    
-    rotateUpClockwise() {
-        this.Up = this.rotateFaceClockwise(this.Up);
-    
-        let frontRow = [...this.Front[0]];
-        let rightRow = [...this.Right[0]];
-        let backRow = [...this.Back[0]];
-        let leftRow = [...this.Left[0]];
-    
-        this.Front[0] = [...rightRow];
-        this.Right[0] = [...backRow];
-        this.Back[0] = [...leftRow];
-        this.Left[0] = [...frontRow];
-    }
-
-    rotateUpCounterClockwise() {
-        this.Up = this.rotateFaceCounterClockwise(this.Up);
-    
-        let frontRow = [...this.Front[0]];
-        let rightRow = [...this.Right[0]];
-        let backRow = [...this.Back[0]];
-        let leftRow = [...this.Left[0]];
-    
-        this.Front[0] = [...leftRow];
-        this.Right[0] = [...frontRow];
-        this.Back[0] = [...rightRow];
-        this.Left[0] = [...backRow];
-    }    
-    
-    rotateDownClockwise() {
-        this.Down = this.rotateFaceClockwise(this.Down);
-    
-        let frontRow = [...this.Front[2]];
-        let rightRow = [...this.Right[2]];
-        let backRow = [...this.Back[2]];
-        let leftRow = [...this.Left[2]];
-
-        this.Front[2] = [...leftRow];
-        this.Right[2] = [...frontRow];
-        this.Back[2] = [...rightRow];
-        this.Left[2] = [...backRow];
-    }
-    
-    rotateDownCounterClockwise() {
-        this.Down = this.rotateFaceCounterClockwise(this.Down);
-    
-        let frontRow = [...this.Front[2]];
-        let rightRow = [...this.Right[2]];
-        let backRow = [...this.Back[2]];
-        let leftRow = [...this.Left[2]];
-    
-        this.Front[2] = [...rightRow];     
-        this.Right[2] = [...backRow];
-        this.Back[2] = [...leftRow];
-        this.Left[2] = [...frontRow];
-    }
-    
-    rotateFrontClockwise() {
-        this.Front = this.rotateFaceClockwise(this.Front);
-    
-        let upRow = this.Up[2].slice();
-        let leftCol = this.Left.map(row => row[2]);
-        let downRow = this.Down[0].slice();
-        let rightCol = this.Right.map(row => row[0]);
-    
-        this.Up[2] = leftCol.reverse();
-        for (let i = 0; i < 3; i++) {
-            this.Left[i][2] = downRow[i]; 
-            this.Down[0][i] = rightCol[i];
-            this.Right[i][0] = upRow.reverse()[i];
-        }
-    }
-    
-    rotateFrontCounterClockwise() {
-        this.Front = this.rotateFaceCounterClockwise(this.Front);
-    
-        let upRow = this.Up[2].slice();
-        let leftCol = this.Left.map(row => row[2]);
-        let downRow = this.Down[0].slice();
-        let rightCol = this.Right.map(row => row[0]);
-    
-        this.Up[2] = rightCol;
-        for (let i = 0; i < 3; i++) {
-            this.Left[i][2] = upRow[i];
-            this.Down[0][i] = leftCol.reverse()[i];
-            this.Right[i][0] = downRow[i];
-        }
-    }
-    
-    rotateBackClockwise() {
-        this.Back = this.rotateFaceClockwise(this.Back);
-    
-        let upRow = this.Up[0].slice();
-        let leftCol = this.Left.map(row => row[0]);
-        let downRow = this.Down[2].slice();
-        let rightCol = this.Right.map(row => row[2]);
-    
-        this.Up[0] = rightCol.reverse();
-        for (let i = 0; i < 3; i++) {
-            this.Left[i][0] = upRow[i];
-            this.Down[2][i] = leftCol.reverse()[i];
-            this.Right[i][2] = downRow[i];
-        }
-    }
-    
-    rotateBackCounterClockwise() {
-        this.Back = this.rotateFaceCounterClockwise(this.Back);
-
-        let upRow = this.Up[0].slice();
-        let leftCol = this.Left.map(row => row[0]);
-        let downRow = this.Down[2].slice();
-        let rightCol = this.Right.map(row => row[2]);
-    
-        this.Up[0] = leftCol.reverse();
-        for (let i = 0; i < 3; i++) {
-            this.Left[i][0] = downRow[i];
-            this.Down[2][i] = rightCol[i];
-            this.Right[i][2] = upRow[i];
-        }
-    }
-    
-    rotateLeftClockwise() {
-        this.Left = this.rotateFaceClockwise(this.Left);
-    
-        let upCol = this.Up.map(row => row[0]);
-        let frontCol = this.Front.map(row => row[0]);
-        let downCol = this.Down.map(row => row[0]);
-        let backCol = this.Back.map(row => row[2]).reverse();
-    
-        for (let i = 0; i < 3; i++) {
-            this.Up[i][0] = backCol[i];
-            this.Front[i][0] = upCol[i];
-            this.Down[i][0] = frontCol[i]; 
-            this.Back[2 - i][2] = downCol[i];
-        }
-    }
-    
-    rotateLeftCounterClockwise() {
-        this.Left = this.rotateFaceCounterClockwise(this.Left);
-        
-        let upCol = this.Up.map(row => row[0]);
-        let frontCol = this.Front.map(row => row[0]);
-        let downCol = this.Down.map(row => row[0]);
-        let backCol = this.Back.map(row => row[2]).reverse();
-        
-        for (let i = 0; i < 3; i++) {
-            this.Up[i][0] = frontCol[i];
-            this.Front[i][0] = downCol[i];
-            this.Down[i][0] = backCol[i];
-            this.Back[2 - i][2] = upCol[i];
-        }
-    }
-
-    rotateRightClockwise() {
-        this.Right = this.rotateFaceClockwise(this.Right);
-    
-        let upCol = this.Up.map(row => row[2]);
-        let frontCol = this.Front.map(row => row[2]);
-        let downCol = this.Down.map(row => row[2]);
-        let backCol = this.Back.map(row => row[0]).reverse();
-    
-        for (let i = 0; i < 3; i++) {
-            this.Up[i][2] = frontCol[i];       
-            this.Front[i][2] = downCol[i];
-            this.Down[i][2] = backCol[i];
-            this.Back[2 - i][0] = upCol[i];
-        }
-    }
-    
-    
-    rotateRightCounterClockwise() {
-        this.Right = this.rotateFaceCounterClockwise(this.Right);
-    
-        let upCol = this.Up.map(row => row[2]);
-        let frontCol = this.Front.map(row => row[2]);
-        let downCol = this.Down.map(row => row[2]);
-        let backCol = this.Back.map(row => row[0]).reverse();
-    
-        for (let i = 0; i < 3; i++) {
-            this.Up[i][2] = backCol[i];
-            this.Front[i][2] = upCol[i];
-            this.Down[i][2] = frontCol[i];
-            this.Back[2 - i][0] = downCol[i]
-        }
-    }  
     
 }
